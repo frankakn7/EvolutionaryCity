35cd59682498f11ffc12af2d70bc7b7dc26172f3
Unknown <d.bonello@gmx.de>
2016-12-13 17:48:04 +0100

    freeShop + factory remove function

--- main.js
+++ main.js
@@ -10,3 +10,11 @@ var freeFactory = [];
 function removeFreeOffice(){
 	freeOffice.splice(0,1);
 }
+
+function removeFreeShop() {
+    freeShop.splice(0, 1);
+}
+
+function removeFreeFactory() {
+    freeFactory.splice(0, 1);
+}
