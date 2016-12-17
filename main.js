7cd4e869b205999e0286e11785add52804843594
Maxim <maxim_st@gmx.de>
2016-12-17 16:12:35 +0100

    Added 50 line To Do list

--- main.js
+++ main.js
@@ -1,3 +1,55 @@
+/* To DO:
+	-Economy
+		-Money system
+	-Person
+		-Automatic Relationships
+		-Automatic reproduction
+		-Death
+		-Health (Small Deseases)
+		-Criminality
+		-Happyness
+		-Social System
+		-Spending Type
+		-Shopping
+		-? Family Disputes
+		- Rich / Poor
+	-Building's
+		-Appartements
+		-Houses (Single Family)
+	-Companys
+		-company types (Construction, Production, Selling) (Only one type)
+			-Entertainment System (Cinema, Theaters)
+		-Mother Company (can own companys of different types)
+			-Subsidiary company's (Tochterfirma) 
+		-Buy Factory's / Shops
+		-Demand Construction of Building
+		-Workplaces
+		-Build Houses
+		-Pay Workers
+		-Logistics
+		-Import / Export
+	-State
+		-taxes
+		-Roads
+			-Upgrade Roads
+		-Schools/education
+		-Health Care (Hospital)
+		-Emergency System (Police,Fire Department, Ambulance)
+		-? pension
+	-Catastrophe's 
+		-Tornados
+		-Epidemics
+	-Other City's
+		-Communication
+		-Airports
+		-Trading
+		-? War
+		
+	<-----------------VISUAL------------->
+		-Pictures (Houses ...)
+		-Movement of Canvas
+*/
+
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
